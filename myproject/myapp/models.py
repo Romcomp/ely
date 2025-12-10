c2a3be11ca8f5436af11f18ebc6f7d0a1d3d68eb
Romari <lawrencetsup@gmail.com>
2025-12-10 20:40:44 +0800

    Initial commit

---
+++ myproject/myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Members(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
